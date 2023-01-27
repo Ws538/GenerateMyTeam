740fcc640359e759f264daa0d6d49f15e86b0918
Ws538 <williamsu668@gmail.com>
2023-01-27 16:17:10 +1100

    updated tests for each class

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,4 +1,24 @@
+const Employee = require('../lib/Employee');
+const assert = require('assert');
 
+describe('Employee', () => {
+    it('should return the correct name', () => {
+        const employee = new Employee("William", 1, "test@test.com");
+        assert.equal(employee.getName(), 'William');
+    });
 
- 
+    it('should return the correct id', () => {
+        const employee = new Employee('William', 1, 'test@test.com');
+        assert.equal(employee.getId(), 1);
+    });
 
+    it('should return the correct email', () => {
+        const employee = new Employee('William', 1, 'test@test.com');
+        assert.equal(employee.getEmail(), 'test@test.com');
+    });
+
+    it('should return the correct role', () => {
+        const employee = new Employee('William', 1, 'test@test.com');
+        assert.equal(employee.getRole(), 'Employee');
+    });
+});
